5076819b0cbb58162a61d31704cef3c696f74bc9
synkd <dsynk@redhat.com>
2022-03-02 17:30:05 -0500

    Modify Ansible Variable create method

--- airgun/entities/ansible_variable.py
+++ airgun/entities/ansible_variable.py
@@ -34,6 +34,8 @@ class AnsibleVariablesEntity(BaseEntity):
     def create(self, values):
         """Create a new Ansible variable"""
         view = self.navigate_to(self, 'New')
+        view.override.fill(True)
+        view.matcher_section.before_fill(values)
         view.fill(values)
         view.submit.click()
         view.flash.assert_no_error()
